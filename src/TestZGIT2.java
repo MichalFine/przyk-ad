790e9a317442d897b273e193161d191bc5812165
Michal Zygor <michal@winami.com>
2020-10-01 22:14:24 +0200

    Poprawka 5454

--- src/TestZGIT2.java
+++ src/TestZGIT2.java
@@ -1,6 +1,6 @@
 public class TestZGIT2 {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("wowo");
+        System.out.println("wowo 4545");
     }
 }
